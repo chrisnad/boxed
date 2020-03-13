bda5ebebb3866b65b5681e668b5e01a223965c7f
cleitus <christiannader@hotmail.com>
2020-03-13 18:22:38 +0100

    base

---
+++ src/main/java/com/pubstack/homework/ws/boxed/BoxedApplication.java
@@ -0,0 +1,13 @@
+package com.pubstack.homework.ws.boxed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoxedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pubstack/homework/ws/boxed/BoxedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pubstack.homework.ws.boxed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
